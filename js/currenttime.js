aebc0683cbe610853853bd63c032a7859377d491
Arturo Lainez <arturojlainez@gmail.com>
2022-02-11 17:43:45 -0500

    update extensions, linters, improve JS

--- js/currenttime.js
+++ js/currenttime.js
@@ -2,85 +2,140 @@
 // Update the count down every 1 second
 
 // Clock
-setInterval(currentTime, 1000);
+setInterval(currentTime, 1000)
 
 function currentTime() {
-   // Get today's date and time
-   const date = new Date();
-   let currentHour = date.getHours();
-   let currentMinute = date.getMinutes();
-   let currentSecond = date.getSeconds();
+	// Get today's date and time
+	const date = new Date()
+	let currentHour = date.getHours()
+	let currentMinute = date.getMinutes()
+	let currentSecond = date.getSeconds()
 
-   // formatting time (to include 0X)
+	// formatting time (to include 0X)
 
-   const dayPeriod = currentHour < 12 ? 'AM' : 'PM';
-   currentHour = currentHour === 0 ? 12 : currentHour;
-   currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
-   currentHour = (currentHour < 10 ? '0' : '') + currentHour;
-   currentMinute = (currentMinute < 10 ? '0' : '') + currentMinute;
-   currentSecond = (currentSecond < 10 ? '0' : '') + currentSecond;
+	const dayPeriod = currentHour < 12 ? "AM" : "PM"
+	currentHour = currentHour === 0 ? 12 : currentHour
+	currentHour = currentHour > 12 ? currentHour - 12 : currentHour
+	currentHour = (currentHour < 10 ? "0" : "") + currentHour
+	currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute
+	currentSecond = (currentSecond < 10 ? "0" : "") + currentSecond
 
-   // Month values
-   let monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-   let month = monthName[date.getMonth()];
+	// Month values
+	let monthName = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	]
+	let month = monthName[date.getMonth()]
 
-   // Weekday values
-   let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-   let weekday = week[date.getDay()];
+	// Weekday values
+	let week = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	]
+	let weekday = week[date.getDay()]
 
-   const year = date.getFullYear();
-   const day = date.getDate();
+	const year = date.getFullYear()
+	const day = date.getDate()
 
-   // Output the result in an element with class current-time
-   document.querySelector('p.current-time span').firstChild.nodeValue = `${currentHour}`;
-   document.querySelector('p.current-time span:nth-child(2)').firstChild.nodeValue = `${currentMinute}`;
-   document.querySelector('p.current-time span:nth-child(3)').firstChild.nodeValue = `${currentSecond}`;
-   document.querySelector('p.current-time span:nth-child(4)').firstChild.nodeValue = `${dayPeriod}`;
+	// Output the result in an element with class current-time
+	document.querySelector(
+		"p.current-time span"
+	).firstChild.nodeValue = `${currentHour}`
+	document.querySelector(
+		"p.current-time span:nth-child(2)"
+	).firstChild.nodeValue = `${currentMinute}`
+	document.querySelector(
+		"p.current-time span:nth-child(3)"
+	).firstChild.nodeValue = `${currentSecond}`
+	document.querySelector(
+		"p.current-time span:nth-child(4)"
+	).firstChild.nodeValue = `${dayPeriod}`
 
-   document.querySelector('.current-date').firstChild.nodeValue = `${weekday}, ${month} ${day}, ${year}`;
+	document.querySelector(
+		".current-date"
+	).firstChild.nodeValue = `${weekday}, ${month} ${day}, ${year}`
 
-   // mini clock
-   document.querySelector('.current-date-small').firstChild.nodeValue = `${weekday}, ${month} ${day}, ${year}`;
-   document.querySelector('.current-time-small').firstChild.nodeValue = `${currentHour}: ${currentMinute} ${dayPeriod}`;
+	// mini clock
+	document.querySelector(
+		".current-date-small"
+	).firstChild.nodeValue = `${weekday}, ${month} ${day}, ${year}`
+	document.querySelector(
+		".current-time-small"
+	).firstChild.nodeValue = `${currentHour}: ${currentMinute} ${dayPeriod}`
 }
-
 
 //Menu Selection
-var timeMenuDiv = ['clock', 'timer', 'stopwatch', 'alarm'];
-var visibleTimeId = null;
-const miniClock = document.getElementById('mini-clock');
-
-
+var timeMenuDiv = ["clock", "timer", "stopwatch", "alarm"]
+var visibleTimeId = null
+const miniClock = document.getElementById("mini-clock")
 
 function divVisibility(divId) {
+	visibleTimeId = divId
+	let i, div
 
-   visibleTimeId = divId;
-   let i, div;
+	for (i = 0; i < timeMenuDiv.length; i++) {
+		div = document.getElementById(timeMenuDiv[i])
 
-   for (i = 0; i < timeMenuDiv.length; i++) {
-      div = document.getElementById(timeMenuDiv[i]);
+		if (visibleTimeId === timeMenuDiv[i]) {
+			div.style.display = "block"
+			div.style.animationDuration = "1s"
+		} else {
+			div.style.display = "none"
+		}
 
-      if (visibleTimeId === timeMenuDiv[i]) {
-         div.style.display = 'block';
-         div.style.animationDuration = '1s';
-
-      } else {
-         div.style.display = 'none';
-      }
-
-      console.log(div, visibleTimeId, div.style.display);
-
-   }
-   if (visibleTimeId !== 'clock') {
-      miniClock.style.display = 'block';
-   } else {
-      miniClock.style.display = 'none';
-   }
+		console.log(div, visibleTimeId, div.style.display)
+	}
+	if (visibleTimeId !== "clock") {
+		miniClock.style.display = "block"
+	} else {
+		miniClock.style.display = "none"
+	}
 }
 
-document.getElementById('timer-btn').addEventListener('click', () => divVisibility('timer'));
-document.getElementById('alarm-btn').addEventListener('click', () => divVisibility('alarm'));
-document.getElementById('stopwatch-btn').addEventListener('click', () => divVisibility('stopwatch'));
-document.getElementById('clock-btn').addEventListener('click', () => divVisibility('clock'));
+document
+	.getElementById("timer-btn")
+	.addEventListener("click", () => divVisibility("timer"))
+document
+	.getElementById("alarm-btn")
+	.addEventListener("click", () => divVisibility("alarm"))
+document
+	.getElementById("stopwatch-btn")
+	.addEventListener("click", () => divVisibility("stopwatch"))
+document
+	.getElementById("clock-btn")
+	.addEventListener("click", () => divVisibility("clock"))
 
+document
+	.querySelector(".theme-block:nth-child(1)")
+	.addEventListener(
+		"click",
+		() =>
+			(document.querySelector("body").style.background =
+				"linear-gradient(0deg, #f12711, #f5af19)"),
+		(document.querySelector("body").style.color = "#000")
+	)
 
+document
+	.querySelector(".theme-block:nth-child(2)")
+	.addEventListener(
+		"click",
+		() =>
+			(document.querySelector("body").style.background =
+				"linear-gradient(#020024 0%, #090979 35%, #00d4ff 100%)"),
+		(document.querySelector("body").style.color = "#ffffff")
+	)
--- js/weather.js
+++ js/weather.js
@@ -1,69 +1,101 @@
 /* jshint esversion: 6 */
 
+var weatherInterval = setInterval(weatherDisplay, 6000)
+
 // weather
-window.addEventListener('load', () => {
-   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-         longitude = position.coords.longitude;
-         latitude = position.coords.latitude;
-         const api = `https://api.weather.gov/points/${latitude},${longitude}`;
+function weatherDisplay() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			longitude = position.coords.longitude
+			latitude = position.coords.latitude
+			const api = `https://api.weather.gov/points/${latitude},${longitude}`
 
-         //console.log(latitude, longitude, api);
+			//console.log(latitude, longitude, api);
 
-         fetch(api) //fetching weather api
-            .then(response => {
-               return response.json();
-            })
-            .then(data => {
-               //console.log(data);
-               const { forecastHourly } = data.properties;
-               const { city, state } = data.properties.relativeLocation.properties;
+			fetch(api) //fetching weather api
+				.then((response) => {
+					return response.json()
+				})
+				.then((data) => {
+					//console.log(data);
+					const { forecastHourly } = data.properties
+					const { city, state } = data.properties.relativeLocation.properties
 
-               document.querySelector(".city").innerHTML = `${city}, `;
-               document.querySelector(".state").innerHTML = state;
-               // console.log(forecastHourly);
+					document.querySelector(".city").innerHTML = `${city}, `
+					document.querySelector(".state").innerHTML = state
+					// console.log(forecastHourly);
 
-               fetch(forecastHourly) //fetching weather
-                  .then(res => {
-                     return res.json();
-                  })
-                  .then(weather => {
-                     // console.log(weather);
+					fetch(forecastHourly) //fetching weather
+						.then((res) => {
+							return res.json()
+						})
+						.then((weather) => {
+							// console.log(weather);
 
-                     const { temperature, temperatureUnit, shortForecast, isDaytime } = weather.properties.periods[0];
+							const { temperature, temperatureUnit, shortForecast, isDaytime } =
+								weather.properties.periods[0]
+							let farenheit = temperature
+							let celsius = Math.floor(((farenheit - 32) * 5) / 9)
 
+							if (
+								document.querySelector(".temp-unit").innerHTML ===
+								temperatureUnit
+							) {
+								document.querySelector(
+									".temperature span:first-child"
+								).innerHTML = `${temperature}&#176;`
+								document.querySelector(".temp-unit").innerHTML = temperatureUnit
+							} else {
+								document.querySelector(".temp-unit").innerHTML = "C"
+								document.querySelector(
+									".temperature span:first-child"
+								).innerHTML = `${celsius}&#176;`
+							}
 
-                     document.querySelector(".temperature span:first-child").innerHTML = `${temperature}&#176;`;
-                     document.querySelector(".temp-unit").innerHTML = temperatureUnit;
-                     document.querySelector(".forecast").innerHTML = shortForecast;
+							document.querySelector(".forecast").innerHTML = shortForecast
 
+							let temperatureContainer = document.querySelector(
+								".temperature-container"
+							)
 
-                     let temperatureContainer = document.querySelector(".temperature-container");
+							temperatureContainer.addEventListener("click", () => {
+								if (
+									document.querySelector(".temp-unit").innerHTML ===
+									temperatureUnit
+								) {
+									document.querySelector(".temp-unit").innerHTML = "C"
+									document.querySelector(
+										".temperature span:first-child"
+									).innerHTML = `${celsius}&#176;`
+								} else {
+									if (document.querySelector(".temp-unit").innerHTML === "C") {
+										document.querySelector(".temp-unit").innerHTML =
+											temperatureUnit
+										document.querySelector(
+											".temperature span:first-child"
+										).innerHTML = `${farenheit}&#176;`
+									}
+								}
+							})
+						})
+						.catch((err) => {
+							console.log(`Fetch Problem(forecastHourly): ${err.message}
+							timestamp: ${new Date()}`)
+							console.log(new Date())
+							clearInterval(weatherInterval)
+						})
+				})
+				.catch((err) => {
+					console.log(`Fetch Problem(API): ${err.message}
+					Coordinates should be inside U.S.
+					Reload to Retry
+					timestamp: ${new Date()}`)
+					document.querySelector(".temperature-container").style.display =
+						"none"
+					clearInterval(weatherInterval)
+				})
+		})
+	}
+}
 
-                     let farenheit = temperature;
-                     let celsius = Math.floor((farenheit - 32) * 5 / 9);
-
-                     temperatureContainer.addEventListener("click", () => {
-                        if (document.querySelector(".temp-unit").innerHTML === temperatureUnit) {
-                           document.querySelector(".temp-unit").innerHTML = 'C';
-                           document.querySelector(".temperature span:first-child").innerHTML = `${celsius}&#176;`;
-                        } else {
-                           if (document.querySelector(".temp-unit").innerHTML === 'C') {
-                              document.querySelector(".temp-unit").innerHTML = temperatureUnit;
-                              document.querySelector(".temperature span:first-child").innerHTML = `${farenheit}&#176;`;
-                           }
-                        }
-                     });
-
-                  })
-                  .catch(err => {
-                     console.log(`Fetch Problem(forecastHourly): ${err.message}`);
-                  });
-            })
-            .catch(err => {
-               console.log(`Fetch Problem(API): ${err.message}`);
-            });
-      });
-   }
-
-});
+window.addEventListener("load", weatherDisplay)
